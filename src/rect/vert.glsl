ba7e1f47296ce35b5120327189552aaf303059af
linsyking <kxiang@umich.edu>
2025-05-22 22:52:02 +0800

    fix: rect

--- src/rect/vert.glsl
+++ src/rect/vert.glsl
@@ -17,11 +17,22 @@ void main() {
     }
 
     // Translate to the rectangle's position
-    vec2 worldPosition = posize.xy + rotatedVertex;
+    vec2 wpos = posize.xy + rotatedVertex;
 
-    // Transform to OpenGL clip space
-    vec2 clipPosition = vec2((worldPosition.x / view.x) * 2.0 - 1.0, 1. - (worldPosition.y / view.y) * 2.0);
 
-    gl_Position = vec4(clipPosition, 0.0, 1.0);
+    if (camera.w == 0.0){
+        // No rotation
+        vec2 pos = (wpos - camera.xy) * camera.z / view;
+        gl_Position = vec4(pos, 0, 1);
+    } else {
+        mat2 rotation = mat2(cos(camera.w), -sin(camera.w), sin(camera.w), cos(camera.w));
+        vec2 pos = (rotation * (wpos - camera.xy)) * camera.z / view;
+        gl_Position = vec4(pos, 0, 1);
+    }
+
+    // // Transform to OpenGL clip space
+    // vec2 clipPosition = vec2((worldPosition.x / view.x) * 2.0 - 1.0, 1. - (worldPosition.y / view.y) * 2.0);
+
+    // gl_Position = vec4(clipPosition, 0.0, 1.0);
 
 }
