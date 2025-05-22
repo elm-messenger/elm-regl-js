3cd9798407c860b049b5e756b1309775e8893373
linsyking <kxiang@umich.edu>
2025-05-22 17:53:36 +0800

    fix: triangle

--- src/triangle/vert.glsl
+++ src/triangle/vert.glsl
@@ -3,11 +3,12 @@ attribute vec2 position;
 uniform vec2 view;
 uniform vec4 camera;
 void main() {
-    if (camera.w == 0){
+    if (camera.w == 0.0){
         // No rotation
-        vec2 pos = (position - camera.xy) * camera.z;
+        vec2 pos = (position - camera.xy) * camera.z / view;
+        gl_Position = vec4(pos, 0, 1);
     } else {
-        mat2 rotation = mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
+        mat2 rotation = mat2(cos(camera.w), -sin(camera.w), sin(camera.w), cos(camera.w));
         vec2 pos = (rotation * (position - camera.xy)) * camera.z / view;
         gl_Position = vec4(pos, 0, 1);
     }
