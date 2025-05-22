aad9f8ee65ff998be80f4eee4c9829bf8f8c84f1
linsyking <kxiang@umich.edu>
2025-05-22 22:04:12 +0800

    feat: cricle camera

--- src/circle/frag.glsl
+++ src/circle/frag.glsl
@@ -2,12 +2,21 @@ precision mediump float;
 uniform vec2 center;
 uniform float radius;
 uniform vec2 view;
+uniform vec4 camera;
 uniform vec4 color;
 varying vec2 v_position;
 void main() {
-    vec2 position = vec2((v_position.x + 1.) / 2. * view.x, (1. - v_position.y ) / 2. * view.y);
+    vec2 position = v_position * view / camera.z;
+    vec2 cpos;
+    if (camera.w == 0.0){
+        // No rotation
+        cpos = (center - camera.xy);
+    } else {
+        mat2 rotation = mat2(cos(camera.w), -sin(camera.w), sin(camera.w), cos(camera.w));
+        cpos = (rotation * (center - camera.xy));
+    }
 
-    float distance = distance(position, center);
+    float distance = distance(position, cpos);
     if(distance > radius + 1.) {
         discard;
     }
