b7ac01704b39d2821a7608fc2e52e61f0d154576
linsyking <kxiang@umich.edu>
2025-06-08 13:32:55 +0800

    fix: shader

--- src/imgFade/frag.glsl
+++ src/imgFade/frag.glsl
@@ -4,13 +4,13 @@ uniform sampler2D t2;
 uniform sampler2D mask;
 uniform float t;
 uniform int invert_mask;
-varying vec2 vuv;
+varying vec2 uv;
 void main() {
-    float t0 = texture2D(mask, vuv).x;
+    float t0 = texture2D(mask, uv).x;
     if(invert_mask == 1) {
         t0 = 1. - t0;
     }
     t0 = t0 * .5 + .5;
     float a = smoothstep(-0.5, 0., (t - t0));
-    gl_FragColor = mix(texture2D(t1, vuv), texture2D(t2, vuv), a);
+    gl_FragColor = mix(texture2D(t1, uv), texture2D(t2, uv), a);
 }
--- src/pixilation/frag.glsl
+++ src/pixilation/frag.glsl
@@ -5,7 +5,7 @@ uniform vec2 view;
 uniform float pixelSize;
 varying vec2 uv;
 void main() {
-    vec2 res = vec2(view.x*2, -view.y*2);
+    vec2 res = vec2(view.x*2., -view.y*2.);
     vec2 puv = floor(uv * res/pixelSize)*pixelSize/res;
     gl_FragColor = texture2D(texture, puv);
 }
