2b527f3696119978b4fe10596a9e777df389dd8b
linsyking <kxiang@umich.edu>
2025-06-12 23:16:43 +0800

    fix: text rendering

--- src/text/frag.glsl
+++ src/text/frag.glsl
@@ -19,19 +19,23 @@ void main() {
     float outline = topt.y;
     vec3 tex = texture2D(tMap, vUv).rgb;
     float d = max(min(tex.r, tex.g), min(max(tex.r, tex.g), tex.b)) - 0.5;
-    float screenPxDistance = screenPxRange() * d;
-    float alpha = clamp(screenPxDistance + thickness, 0.0, 1.0);
+    float bodyDist = screenPxRange() * d;
+    float alpha = clamp(bodyDist + thickness, 0.0, 1.0);
     if (outline > 0.) {
         vec4 outlineColor = vec4(ocolor.rgb * ocolor.a, ocolor.a);
-        float outlineAlpha = clamp(screenPxDistance + outline, 0.0, 1.0);
+        float outlineAlpha = clamp(bodyDist + thickness + outline, 0.0, 1.0);
         if(outlineAlpha < 0.01) discard;
-        if (alpha < 0.01) {
-            // Outline!
-            gl_FragColor = outlineColor * outlineAlpha;
-        }else{
-            // Normal
-            gl_FragColor = nc * alpha;
+        vec3 mcolor;
+        float nalpha;
+        if (alpha > 0.5) {
+            mcolor = nc.rgb * alpha;
+            nalpha = alpha * nc.a;
+        } else {
+            mcolor = outlineColor.rgb * outlineAlpha;
+            nalpha = outlineAlpha * outlineColor.a;
         }
+
+        gl_FragColor = vec4(mcolor, nalpha);
     } else {
         if(alpha < 0.01) discard;
         gl_FragColor = nc * alpha;
